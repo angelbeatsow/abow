142bc84d80ac42b7338e8327e31825cdd41f72dc
angelbeatsow <117999333+angelbeatsow@users.noreply.github.com>
2022-11-13 00:49:07 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-import java.util.Random;
+
 const fire = 'f';
 const water = 'w';
 const grass = 'g';
@@ -16,15 +16,75 @@ const IDS = [
 ]
 
 let isRun = true;
+let isRunning = false;
+let Rnumber = 1;
 
 function $(id){
   return document.getElementById(id);
 }
 
-function randomNumber(){
-    Random random = new Random();　
-    let n =　random.nextInt(6);
-    return n;    
+function randomNumber(){　
+    Rnumber = Rnumber + 1;
+    return Rnumber % 6;    
+}
+
+function suujihenkan(suuji){
+  for(let aunt = 1;aunt < 37;aunt++){
+    if(suuji == 'b0' + aunt ||suuji == 'b' + aunt){
+      return aunt;
+    }
+  }
+}
+
+function clickAction(event){
+  if(!isRun){
+    return;
+  }
+  event.preventDefault();
+  let id = event.target.id;
+  let object = $(id).value;
+  let lastPoint = [id,object];
+  let isDissaper = false;
+  let sentaku = 0;
+  let sentakuList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  isRun = false;
+  isRunning = true;
+  $(id).value = 'x';
+  console.log(lastPoint[0])
+  console.log(lastPoint[1])
+  console.log(window.event.buttons)
+    window.addEventListener('touchend',function(event){
+      if(isDissaper){
+        
+      }else{
+        $(lastPoint[0]).value = lastPoint[1];
+        isRun = true;
+        isRunning = false;
+      }
+    })
+  
+}
+
+function untilClick(event){
+  if(!isRunning){
+    return;
+  }
+  let idid = event.target.id;
+  let suujihenkanidid = suujihenkan(idid)
+  console.log(suujihenkanidid)
+  if(suujihenkan(idid) == suujihenkan(lastPoint[0]) - 7 ||suujihenkan(idid) == suujihenkan(lastPoint[0]) - 6 ||suujihenkan(idid) == suujihenkan(lastPoint[0]) - 5 ||
+  suujihenkan(idid) == suujihenkan(lastPoint[0]) - 1 ||suujihenkan(idid) == suujihenkan(lastPoint[0]) + 1 ||suujihenkan(idid) == suujihenkan(lastPoint[0]) + 5 ||
+  suujihenkan(idid) == suujihenkan(lastPoint[0]) + 6 ||suujihenkan(idid) == suujihenkan(lastPoint[0]) + 7){
+    if($(idid).value == $(lastPoint[1]).value){
+      lastPoint[0] = idid;
+      isDissaper = true;
+      $(idid).value = 'x'
+      sentakuList[sentaku] = idid;
+      sentaku++;
+    }else{
+      return;}
+  }else{
+    return;}
 }
 
 function onloadAction(){
@@ -32,7 +92,10 @@ function onloadAction(){
     for(let col=0;col < 6;col++){
       let randoma = randomNumber();
       $(IDS[row][col]).value = droplist[randoma];
+      $(IDS[row][col]).addEventListener('touchstart',clickAction);
+      $(IDS[row][col]).mouseEntered = untilClick;
     }}
-}
+    }
+
 
 window.onload = onloadAction();
