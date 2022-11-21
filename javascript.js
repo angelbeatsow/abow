de3ab3ccc6c444e2e87a2068772c4eaff46a7da5
angelbeatsow <117999333+angelbeatsow@users.noreply.github.com>
2022-11-22 01:43:07 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -161,7 +161,7 @@ function clickendAction(event){
 	         timeCount = true;
               }else if($('timegauge').style.width != '1px'){
 	      //消したブロックに応じてカウントを増やす
-		     gaugewidth = gaugewidth + howManyDissaper * 10 ;
+		     gaugewidth = gaugewidth + howManyDissaper * 20 ;
 		    $('timegauge').style.width = gaugewidth + 'px';
 	      }
    
