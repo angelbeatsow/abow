9e7b01211338925b2c091652d32938b73164c3de
angelbeatsow <117999333+angelbeatsow@users.noreply.github.com>
2022-11-23 04:54:31 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -64,11 +64,11 @@ function suujihenkan(suuji){
 
 //ツイート内容の更新
 function tweetKoushin(){
-   $('tweet').dataText = '経過ターン数:' + spendedTurn + 'ターン' +
-                          '消したブロック数合計:' + totalDissaper + '個' +
-                          '一度に消したブロック最大数:' + saidaiDissaper + '個(平均:' + $('sensekiRensaHeikin').innerHTML + '個)' +
-	                  '最大コンボ数:' + saidaiCombo + 'コンボ(平均:' + $('sensekiComboHeikin').innerHTML +'コンボ)' +
-	                  'ABOW(仮)';}
+   $('tweet').setAttribute('data-text','経過ターン数:' + spendedTurn + 'ターン' +
+                          '　消したブロック数合計:' + totalDissaper + '個' +
+                          '　一度に消したブロック最大数:' + saidaiDissaper + '個(平均:' + $('sensekiRensaHeikin').innerHTML + '個)' +
+	                  '　最大コンボ数:' + saidaiCombo + 'コンボ(平均:' + $('sensekiComboHeikin').innerHTML +'コンボ)' +
+	                  '　ABOW(仮)');}
 
 //カウントを減らす処理
 let sousajikan = 5;
